3a145e25b136671081baba41c8996e5f2955d30c
Yu-Cheng Shen <hvgmbdmg@gmail.com>
2017-05-31 11:16:14 +0800

    Simple Table Airport Group Part Finish

--- SimpleTable/SimpleTable/ViewController.swift
+++ SimpleTable/SimpleTable/ViewController.swift
@@ -12,13 +12,32 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     var isLoadData: Bool = false;
     var airportsData = [[String]]();
     
+    struct airportsGrouped {
+        var countryName :String
+        var airportInfo :[[String]]
+    }
+    
+    var groupedArray = [airportsGrouped]()
+    var CountryArray = [String]()
+    
     @IBOutlet weak var tableView: UITableView!
     
     //var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "AAA", "BBB", "CCC", "DDD", "EEE", "FFF", "GGG", "HHH", "III", "JJJ", "KKK", "LLL", "MMM", "NNN", "OOO", "PPP"]
     
+    
+    
     func tableView(_ tableiew: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return airportsData.count
+        return groupedArray[section].airportInfo.count
     }
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return CountryArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return CountryArray[section]
+    }
+
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cellIdentifier = "AirportCell"
@@ -26,12 +45,9 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
         //set cell
 
-        
-        cell.FullName.text = airportsData[indexPath.row][0]
-
-        cell.IATA.text = airportsData[indexPath.row][2]
-        cell.City.text = airportsData[indexPath.row][3]
-
+        cell.FullName.text = groupedArray[indexPath.section].airportInfo[indexPath.row][0]
+        cell.IATA.text = groupedArray[indexPath.section].airportInfo[indexPath.row][2]
+        cell.City.text = groupedArray[indexPath.section].airportInfo[indexPath.row][3]
 
         //cell.textLabel?.text = airportsData[indexPath.row][0]
         //cell.imageView?.image = UIImage(named: "picOne")
@@ -57,8 +73,31 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
             //print(airport["ServedCity"]!)
             
             airportsData.append([airport["Airport"] as! String, airport["Country"] as! String, airport["IATA"] as! String, airport["ServedCity"] as! String])
-        
+            
+            if !CountryArray.contains(airport["Country"] as! String) {
+                CountryArray.append(airport["Country"] as! String)
+            }
         }
+        
+        //then, group airports by country
+        //if
+        
+        for country in CountryArray{
+            var sameCountryArray = [[String]]()
+            sameCountryArray.removeAll()
+            
+            for airport in airportsData{
+                if country == airport[1] {
+                    sameCountryArray.append(airport)
+                }
+            }
+            groupedArray.append(airportsGrouped(countryName: country,airportInfo: sameCountryArray))
+            
+            print(country)
+            //sameCountryArray.removeAll()
+
+        }
+        print(CountryArray.count)
     
     }
 
@@ -73,11 +112,11 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
             if let IndexPath = tableView.indexPathForSelectedRow {
                 let destViewController = segue.destination as! AirportDetailViewController;
                 //let AAA: String = airportsData[IndexPath.row][2]
-                print(airportsData[IndexPath.row][2])
-                destViewController.airportImageName = airportsData[IndexPath.row][2];
-                destViewController.FullNameText = airportsData[IndexPath.row][0]
-                destViewController.CityText = airportsData[IndexPath.row][3]
-                destViewController.CountryText = airportsData[IndexPath.row][1]
+                print(groupedArray[IndexPath.section].airportInfo[IndexPath.row][2])
+                destViewController.airportImageName = groupedArray[IndexPath.section].airportInfo[IndexPath.row][2];
+                destViewController.FullNameText = groupedArray[IndexPath.section].airportInfo[IndexPath.row][0]
+                destViewController.CityText = groupedArray[IndexPath.section].airportInfo[IndexPath.row][3]
+                destViewController.CountryText = groupedArray[IndexPath.section].airportInfo[IndexPath.row][1]
                 
                 
 
