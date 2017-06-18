6883eb724cb1ddc963ee9ea1aeed9a4c9ead81c2
Yu-Cheng Shen <hvgmbdmg@gmail.com>
2017-06-18 18:10:52 +0800

    Add Final Project From Moonlight
    Use Stupid way to solve the problem with change word size.

---
+++ Swift-Final-Froject/AAA/ModelController.swift
@@ -0,0 +1,299 @@
+//
+//  ModelController.swift
+//  AAA
+//
+//  Created by Mac on 2017/6/5.
+//  Copyright © 2017年 MAP_First. All rights reserved.
+//
+
+import UIKit
+
+/*
+ A controller object that manages a simple model -- a collection of month names.
+ 
+ The controller serves as the data source for the page view controller; it therefore implements pageViewController:viewControllerBeforeViewController: and pageViewController:viewControllerAfterViewController:.
+ It also implements a custom method, viewControllerAtIndex: which is useful in the implementation of the data source methods, and in the initial configuration of the application.
+ 
+ There is no need to actually create view controllers for each page in advance -- indeed doing so incurs unnecessary overhead. Given the data model, these methods create, configure, and return a new view controller on demand.
+ */
+
+
+class ModelController: NSObject, UIPageViewControllerDataSource {
+    
+    
+    var textOneLineLength = ViewController.bigWordOneLine
+    var textPageLineNumber = ViewController.bigStringPageLine
+    
+    var smallTextOneLineLength = ViewController.smallWordOneLine
+    var smallTextPageLineNumber = ViewController.smallStringPageLine
+    
+    var rawData: String = ""
+    var lineData: [String] = []
+    var onePageData :String = ""
+    static var pageData: [String] = []
+    
+    var smallLineData: [String] = []
+    var smallOnePageData: String = ""
+    static var smallPageData: [String] = []
+    
+    static var bigIndex:Int = 0;
+    static var smallIndex:Int = 0;
+    
+    
+    //public static var pageData: [String] = []
+    static var filename = ""
+    
+    var textData: [String] = []
+    
+    override init() {
+        super.init()
+        
+        let storageURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let aaa = storageURL.appendingPathComponent("\(ModelController.filename).txt")
+        if let path = Bundle.main.path(forResource: "狼與辛香料第一集片段", ofType: "txt") {
+            do {
+                let data = try String(contentsOfFile: aaa.path, encoding: .utf8)
+                let myStrings = data.components(separatedBy: .newlines)
+                rawData = myStrings.joined(separator: "\n")
+            } catch {
+                print(error)
+            }
+            print("load file finished")
+            
+        }
+        
+        var rawDataLineArr = rawData.characters.split{$0 == "\n"}.map(String.init)
+        
+        for rawLine in rawDataLineArr{
+            //print("====================")
+            //print(rawLine)
+            //print(rawLine.characters.count)
+            
+            var str :String = rawLine
+            while str != "" {
+                let strLen = str.characters.count;
+                if strLen >= textOneLineLength {
+                    let index = str.index(str.startIndex, offsetBy: textOneLineLength)
+                    //print(str.substring(to: index))
+                    lineData.append(str.substring(to: index))
+                    str = String( str.characters.suffix( strLen - textOneLineLength ))
+                }
+                else{
+                    //print(str);
+                    lineData.append(str)
+                    str = ""
+                    lineData.append(" ")
+                }
+            }
+            //print(textLabel.sizeToFit(fullNameArr[i]))
+            //print(ModelController.pageData)
+        }
+        /*
+         for line in lineData{
+         print(line)
+         }
+         */
+        print("line Data Count: ")
+        print(lineData.count)
+        let totalPageNumber = Int( lineData.count / textPageLineNumber ) + 1
+        print("Total Page Number: ")
+        print(totalPageNumber)
+        
+        for i in 0...totalPageNumber-2 {
+            onePageData = ""
+            for j in 0...textPageLineNumber-1 {
+                onePageData.append(lineData[i*textPageLineNumber+j])
+                onePageData.append("\n")
+                //pageData[i].append(lineData[i*23+j])
+            }
+            ModelController.pageData.append(onePageData)
+        }
+        onePageData = ""
+        for i in (totalPageNumber-1)*textPageLineNumber...lineData.count-1{
+            onePageData.append(lineData[i])
+            onePageData.append("\n")
+        }
+        for _ in 0...textPageLineNumber{
+            onePageData.append("\n")
+        }
+        //onePageData.append("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+        ModelController.pageData.append(onePageData)
+        
+        
+        print("doing small file")
+        
+        rawDataLineArr = rawData.characters.split{$0 == "\n"}.map(String.init)
+        
+        for rawLine in rawDataLineArr{
+            //print("====================")
+            //print(rawLine)
+            //print(rawLine.characters.count)
+            
+            var str :String = rawLine
+            while str != "" {
+                let strLen = str.characters.count;
+                if strLen >= smallTextOneLineLength {
+                    let index = str.index(str.startIndex, offsetBy: smallTextOneLineLength)
+                    //print(str.substring(to: index))
+                    smallLineData.append(str.substring(to: index))
+                    str = String( str.characters.suffix( strLen - smallTextOneLineLength ))
+                }
+                else{
+                    //print(str);
+                    smallLineData.append(str)
+                    str = ""
+                    smallLineData.append(" ")
+                }
+            }
+            //print(textLabel.sizeToFit(fullNameArr[i]))
+            //print(ModelController.pageData)
+        }
+        /*
+         for line in lineData{
+         print(line)
+         }
+         */
+        print("small line Data Count: ")
+        print(smallLineData.count)
+        let smallTotalPageNumber = Int( smallLineData.count / smallTextPageLineNumber ) + 1
+        print("Small Total Page Number: ")
+        print(smallTotalPageNumber)
+        
+        for i in 0...smallTotalPageNumber-2 {
+            smallOnePageData = ""
+            for j in 0...smallTextPageLineNumber-1 {
+                smallOnePageData.append(smallLineData[i*smallTextPageLineNumber+j])
+                smallOnePageData.append("\n")
+                //pageData[i].append(lineData[i*23+j])
+            }
+            ModelController.smallPageData.append(smallOnePageData)
+        }
+        smallOnePageData = ""
+        for i in (smallTotalPageNumber-1)*smallTextPageLineNumber...smallLineData.count-1{
+            smallOnePageData.append(smallLineData[i])
+            smallOnePageData.append("\n")
+        }
+        for _ in 0...smallTextPageLineNumber{
+            smallOnePageData.append("\n")
+        }
+        //onePageData.append("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+        ModelController.smallPageData.append(smallOnePageData)
+    }
+    
+    public func doSomething(){
+        print("QQ")
+        if( ViewController.isBig ){//small to big
+            ModelController.bigIndex = ModelController.smallIndex*ModelController.pageData.count/ModelController.smallPageData.count
+            ViewController.changeSizeText = ModelController.pageData[ModelController.bigIndex]
+            
+        }
+        else{
+            ModelController.smallIndex = ModelController.bigIndex*ModelController.smallPageData.count/ModelController.pageData.count;
+            ViewController.changeSizeText = ModelController.smallPageData[ModelController.smallIndex]
+        }
+        //ViewController.isBig
+        
+    }
+    
+    func viewControllerAtIndex(_ index: Int, storyboard: UIStoryboard) -> ViewController? {
+        // Return the data view controller for the given index.
+        print("AT index")
+        if( ViewController.isBig ){
+            if (ModelController.pageData.count == 0) || (index >= ModelController.pageData.count) {
+                return nil
+            }
+        
+            // Create a new view controller and pass suitable data.
+            let dataViewController = storyboard.instantiateViewController(withIdentifier: "TextViewController") as! ViewController
+            dataViewController.dataObject = "狼與辛香料第一集片段"
+            dataViewController.textObject = ModelController.pageData[index]
+            return dataViewController
+        }
+        else{
+            if (ModelController.smallPageData.count == 0) || (index >= ModelController.smallPageData.count) {
+                return nil
+            }
+            
+            // Create a new view controller and pass suitable data.
+            let dataViewController = storyboard.instantiateViewController(withIdentifier: "TextViewController") as! ViewController
+            dataViewController.dataObject = "狼與辛香料第一集片段"
+            dataViewController.textObject = ModelController.smallPageData[index]
+            return dataViewController
+        }
+    }
+    
+    func indexOfViewController(_ viewController: ViewController) -> Int {
+        // Return the index of the given data view controller.
+        // For simplicity, this implementation uses a static array of model objects and the view controller stores the model object; you can therefore use the model object to identify the index.
+        print("index of")
+        if( ViewController.isBig ){
+            return ModelController.pageData.index(of: viewController.textObject) ?? NSNotFound
+        }
+        else{
+            return ModelController.smallPageData.index(of: viewController.textObject) ?? NSNotFound
+        }
+        
+    }
+    
+    // MARK: - Page View Controller Data Source
+    
+    //Turn Left
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        var index = self.indexOfViewController(viewController as! ViewController)
+        if (index == 0) || (index == NSNotFound) {
+            return nil
+        }
+        
+        index -= 1
+        ModelController.bigIndex = index
+        ModelController.smallIndex = index
+        return self.viewControllerAtIndex(index, storyboard: viewController.storyboard!)
+
+    }
+    
+    
+    //Turn Right
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        print("Turn Right")
+        if( ViewController.isBig ){
+            var index = self.indexOfViewController(viewController as! ViewController)
+            if index == NSNotFound {
+                index = ModelController.bigIndex
+                //return nil
+            }
+            index += 1
+        
+            //Test
+            //print(ModelController.pageData[index]);
+            ModelController.bigIndex = index;
+        
+            if index == ModelController.pageData.count {
+                    return nil
+            }
+            print("Turn Right??")
+            return self.viewControllerAtIndex(index, storyboard: viewController.storyboard!)
+        }
+        else{
+            var index = self.indexOfViewController(viewController as! ViewController)
+            if index == NSNotFound {
+                print("HERE!!!")
+                index = ModelController.smallIndex
+                //return nil
+            }
+            print("Where")
+            index += 1
+            
+            ModelController.smallIndex = index;
+            //Test
+            //print(ModelController.pageData[index]);
+            
+            if index == ModelController.smallPageData.count {
+                return nil
+            }
+            print("Turn Right?")
+            return self.viewControllerAtIndex(index, storyboard: viewController.storyboard!)
+        }
+        //print("Turn Right End")
+    }
+}
+
---
+++ Swift-Final-Froject/AAA/textUITableViewController.swift
@@ -0,0 +1,178 @@
+//
+//  textUITableViewController.swift
+//  AAA
+//
+//  Created by Mac on 2017/5/20.
+//  Copyright © 2017年 MAP_First. All rights reserved.
+//
+
+import UIKit
+
+class textUITableViewController: UITableViewController,UITextFieldDelegate{
+    static var selectText:String = ""
+    
+    //File manipulation
+    var noteTitles:[String] = PureTextNote.titleOfSavedNotes()
+    func updateNoteTitles() {
+        self.noteTitles = PureTextNote.titleOfSavedNotes()
+    }
+    var newNote = PureTextNote()
+    var cont:String = ""
+    var tit: String = ""
+    //File manipulation
+    
+    private var myTextField: UITextField!
+    private var fileNameField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView,
+                            numberOfRowsInSection section: Int) -> Int {
+        return self.noteTitles.count
+    }
+    
+    override func tableView(_ tableView: UITableView,
+                            cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "textTitleCell", for: indexPath)
+        let noteTitle = cell.viewWithTag(1000) as! UILabel//table cell的標頭
+        noteTitle.text = self.noteTitles[indexPath.row] as String
+        return cell
+    }
+    
+
+    //使用者選擇的table欄位時紀錄選的是哪一欄
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let selectFile = noteTitles
+        textUITableViewController.selectText = selectFile[indexPath.row]
+        
+        
+    }
+    
+    
+    
+    //Read File
+    @IBAction func readAlink(_ sender: Any) {
+        // UITextFieldの配置するx,yと幅と高さを設定.
+        let tWidth: CGFloat = 200
+        let tHeight: CGFloat = 30
+        let posX: CGFloat = (self.view.bounds.width - tWidth)/2
+        let posY: CGFloat = (self.view.bounds.height - tHeight)/4
+        let posY2: CGFloat = (self.view.bounds.height - tHeight)/5
+        // UITextFieldを作成する.
+        myTextField = UITextField(frame: CGRect(x: posX, y: posY, width: tWidth, height: tHeight))
+        fileNameField = UITextField(frame: CGRect(x: posX, y: posY2, width: tWidth, height: tHeight))
+        // 表示する文字を代入する.
+        //myTextField.text = "https://cdn.fbsbx.com/v/t59.2708-21/18221358_1324177670952874_5554888448973733888_n.txt/kino.txt?oh=e22b7d7a33edc9cbf60bf1ca64f6fb8f&oe=5942A6CC&dl=1"
+        myTextField.text = "https://lookaside.fbsbx.com/file/test.txt?token=AWwdeHv4BzsoDfx8vry7pPDrtMccdFLDUWsl-46wywfX-fkWUlDNZqSsmrIRpuioBYWTD8vOj30wIuhJKEObBoFnPkk-gih2bXcKCMufdfIq0VRYSPgC-PcDHlwqRHLK4fGKARwabcOcVjy6F1rqtxecVF6uD1k5_BpuolklfY21EE1pcBGXWrAvO4J3Qrj4I8I"        
+        fileNameField.text = "File Name"
+        // Delegateを自身に設定する
+        myTextField.delegate = self
+        fileNameField.delegate = self
+        // 枠を表示する.
+        myTextField.borderStyle = .roundedRect
+        fileNameField.borderStyle = .roundedRect
+        // クリアボタンを追加.
+        myTextField.clearButtonMode = .whileEditing
+        myTextField.backgroundColor = UIColor.lightGray
+        myTextField.tag = 1001
+        fileNameField.clearButtonMode = .whileEditing
+        fileNameField.backgroundColor = UIColor.lightGray
+        fileNameField.tag = 1002
+        // Viewに追加する
+        self.view.addSubview(myTextField)
+        self.view.addSubview(fileNameField)
+    }
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        print("textFieldDidBeginEditing: \(textField.text!)")
+    }
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if textField.tag == 1002{
+            self.newNote.title = textField.text!
+        }
+        print("textFieldDidEndEditing: \(textField.text!)")
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print("textFieldShouldReturn \(textField.text!)")
+        // 改行ボタンが押されたらKeyboardを閉じる処理.
+        textField.resignFirstResponder()
+        let readIn = self.myTextField.text
+        if self.fileNameField.text != "Cancel"{
+            print(readIn!)
+            //let test = "https://cdn.fbsbx.com/v/t59.2708-21/18219948_1324960664207908_5163162044026847232_n.txt/the-ultimate-crisis.txt?oh=c9c901a49bd0a16f661ab3cafa37d6ca&oe=59411496&dl=1"
+            if self.fileNameField.text == "Delete"{
+                do{
+                    try deleteFile(name: readIn!)
+                }
+                catch{
+                    print("Delete file error.")
+                }
+            }
+            else if readIn != ""{
+                do{
+                    try readContent(link: URL(string:readIn!)!)
+                }
+                catch{
+                    print("Read file error.")
+                }
+            }
+            else{
+                print("No URL is written in.")
+            }
+        }
+        if let viewWithTag = self.view.viewWithTag(1001) {
+            viewWithTag.removeFromSuperview()
+        }else{
+            print("No! 1001")
+        }
+        if let viewWithTag = self.view.viewWithTag(1002) {
+            viewWithTag.removeFromSuperview()
+        }else{
+            print("No! 1002")
+        }
+        return true
+    }
+    func readContent (link:URL)throws{
+        cont = try String(contentsOf: link)
+        self.newNote.content = cont
+        self.newNote.title = self.fileNameField.text!
+        if (self.newNote.title == ""){
+            self.newNote.title = PureTextNote.defaultTitle()
+        }
+        try? self.newNote.save()
+        self.updateNoteTitles()
+        if self.isViewLoaded {
+            self.tableView.reloadData()
+        }
+    }
+    func deleteFile (name:String) throws{
+        try PureTextNote.remove(title: name)
+        self.updateNoteTitles()
+        if self.isViewLoaded {
+            self.tableView.reloadData()
+        }
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "OpenFile" {
+            guard let cell = sender as? UITableViewCell else {
+                fatalError("Mis-configured storyboard! The sender should be a cell.")
+            }
+            self.prepareOpeningFile(for: segue, sender: cell)
+        }else {
+            super.prepare(for: segue, sender: sender)
+        }
+    }
+    func prepareOpeningFile(for segue: UIStoryboardSegue, sender: UITableViewCell) {
+        let senderIndexPath = self.tableView.indexPath(for: sender)!
+        let selectedTitle = self.noteTitles[senderIndexPath.row]
+        ModelController.filename = selectedTitle
+    }
+    //Read File
+}
